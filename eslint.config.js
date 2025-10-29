ea385c2cff0258a54aa15065e1c33f64a75720de
Ruben Shalimov <r_shalimov@inbox.ru>
2025-10-29 19:13:58 +0300

    mst-related packages added.

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,35 @@
 import parentConfig from '@robinbobin/eslint-config-react'
 import { defineConfig } from 'eslint/config'
 
-export default defineConfig(parentConfig)
+export default defineConfig([
+  ...parentConfig,
+  {
+    rules: {
+      '@typescript-eslint/no-invalid-void-type': [
+        'error',
+        {
+          allowAsThisParameter: true
+        }
+      ],
+      '@typescript-eslint/prefer-readonly-parameter-types': [
+        'error',
+        {
+          allow: [
+            {
+              from: 'package',
+              name: ['RecursiveArray', 'RegisteredStyle', 'ViewStyle'],
+              package: 'react-native'
+            }
+          ],
+          ignoreInferredTypes: true
+        }
+      ]
+    }
+  },
+  {
+    files: ['./src/mst/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-empty-object-type': 'off'
+    }
+  }
+])
