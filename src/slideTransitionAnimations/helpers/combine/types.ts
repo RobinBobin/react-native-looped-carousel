ee6c20e1c05e2367681d0a500bae3e5bbdbf0624
Ruben Shalimov <r_shalimov@inbox.ru>
2025-11-10 23:27:48 +0300

    `src/slideTransitionAnimations/helpers/combine/types.ts`: `TCombineReturnType` fixed.

--- src/slideTransitionAnimations/helpers/combine/types.ts
+++ src/slideTransitionAnimations/helpers/combine/types.ts
@@ -1,22 +1,19 @@
 type TPartialReturnType<T1, T2> = T1 & (T2 extends undefined ? T1 : T2)
 
-export type TCombineReturnType<T, S1, S2, S3, S4, S5, S6, S7, S8, S9> =
+export type TCombineReturnType<T, S1, S2, S3, S4, S5, S6, S7, S8, S9> = T &
   TPartialReturnType<
-    T,
+    S1,
     TPartialReturnType<
-      S1,
+      S2,
       TPartialReturnType<
-        S2,
+        S3,
         TPartialReturnType<
-          S3,
+          S4,
           TPartialReturnType<
-            S4,
+            S5,
             TPartialReturnType<
-              S5,
-              TPartialReturnType<
-                S6,
-                TPartialReturnType<S7, TPartialReturnType<S8, S9>>
-              >
+              S6,
+              TPartialReturnType<S7, TPartialReturnType<S8, S9>>
             >
           >
         >
